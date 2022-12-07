8f4510459f1a86d7a611b6792a5193ae7126c3dd
Imran <imran.ali@iqvia.com>
2022-12-08 01:24:49 +0600

    hunt

--- js/script.js
+++ js/script.js
@@ -141,6 +141,36 @@ $(function(){
     arrows: false,
     // nextArrow:".ne",
     //  prevArrow:".pr",
+
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
   });
 
 })
